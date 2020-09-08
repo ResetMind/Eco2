650c21b692c8bd5a931683be7493bbfa61080c9b
ResetMind <avmakarov.email@gmail.com>
2020-09-08 22:10:18 +0300

    shit

--- js/table_engine.js
+++ js/table_engine.js
@@ -4,6 +4,7 @@ function setTableEngine(n) {
     let table_header = document.querySelector(".table" + n + "_header");
     let table_body = document.querySelector(".table" + n + "_body");
     let content = document.querySelectorAll(".content")[n - 1];
+    console.log(content);
     content.onscroll = function() {
         console.log(content.scrollLeft);
     }
