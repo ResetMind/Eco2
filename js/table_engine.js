676436a53ccdc5e12570e06184078d85ae25819d
ResetMind <avmakarov.email@gmail.com>
2020-09-20 21:46:01 +0300

    shit

--- js/table_engine.js
+++ js/table_engine.js
@@ -1,11 +1,18 @@
+let horiz_stub_up = document.querySelector(".horiz_stub_up");
+let header = document.querySelector("header");
+let footer = document.querySelector("footer");
 setTableEngine(1);
 
 function setTableEngine(n) {
     let table_header = document.querySelector(".table" + n + "_header");
     let table_body = document.querySelector(".table" + n + "_body");
     let content = document.querySelectorAll(".content")[n - 1];
-    console.log(content);
+    let wrapper = document.querySelectorAll(".wrapper")[n - 1];
+    /*content.style.height = document.documentElement.clientHeight - horiz_stub_up.clientHeight * 2 - header.clientHeight * 2 - 22 + "px";*/
+    /*content.style.width = document.documentElement.clientWidth + "px";*/
+    /*console.log(content.clientWidth);
+    console.log(content.offsetWidth - content.clientWidth);*/
     content.onscroll = function() {
-        console.log(content.scrollLeft);
+        /*console.log(content.scrollLeft);*/
     }
 }
