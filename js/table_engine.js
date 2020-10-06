bbff86febdf9cfca056e5ffd252ccf6b8dd074ba
ResetMind <avmakarov.email@gmail.com>
2020-10-07 00:18:15 +0300

    shit

--- js/table_engine.js
+++ js/table_engine.js
@@ -1,4 +1,5 @@
 let horiz_stub_up = document.querySelector(".horiz_stub_up");
+let horiz_stub_bottom = document.querySelector(".horiz_stub_bottom");
 let vert_stub_left = document.querySelector(".vert_stub_left");
 let vert_stub_right = document.querySelector(".vert_stub_right");
 let header = document.querySelector("header");
@@ -36,21 +37,86 @@ function onTdSelect(tds, ths, cnt_i) {
         }
         tds[i].onmousedown = function(e) {
             let scroll_interval = null;
+            let last_target = null;
+            let last_pos = "null";
             let x_start = e.clientX;
             let y_start = e.clientY;
             selected_content = content[cnt_i];
             selected_td = tds[i];
             drawCellRect(tds[i], content[cnt_i]);
+            /*Плавный скролл таблицы, когда драгаешь ячейку*/
             document.onmousemove = function(e) {
-                if(e.target == vert_stub_right && scroll_interval == null) {
-                    console.log(vert_stub_right);
-                    scroll_interval = setInterval(() => selected_content.scrollBy(tds[i].getBoundingClientRect().width, 0), 250);
+                e.preventDefault();
+                //console.log(e.clie);       
+                /*if (e.target == vert_stub_right) {
+                    if (scroll_interval == null) {
+                        scroll_interval = scrollOnDrag(0, tds[i].getBoundingClientRect().width, selected_content, 250);
+                    }
+                } else if (e.target == vert_stub_left) {
+                    if (scroll_interval == null) {
+                        scroll_interval = scrollOnDrag(0, -tds[i].getBoundingClientRect().width, selected_content, 250);
+                    }
+                } else if (e.target == table_header[cnt_i] || table_header[cnt_i].contains(e.target) || e.target == horiz_stub_up) {
+                    if (scroll_interval == null || last_target == header) {
+                        clearInterval(scroll_interval);
+                        scroll_interval = scrollOnDrag(-tds[i].getBoundingClientRect().height, 0, selected_content, 250);
+                    }
+                } else if (e.target == header) {
+                    if (scroll_interval == null || last_target == horiz_stub_up || last_target == table_header[cnt_i] || table_header[cnt_i].contains(last_target)) {
+                        clearInterval(scroll_interval);
+                        scroll_interval = scrollOnDrag(-tds[i].getBoundingClientRect().height, 0, selected_content, 150);
+                    }
+                } else if (e.target == horiz_stub_bottom) {
+                    if (scroll_interval == null) {
+                        scroll_interval = scrollOnDrag(tds[i].getBoundingClientRect().height, 0, selected_content, 250);
+                    }
+                } else {
+                    clearInterval(scroll_interval);
+                    scroll_interval = null;
+                }*/
+                let scroll_width = selected_content.offsetWidth - selected_content.clientWidth > 0 ? selected_content.offsetWidth - selected_content.clientWidth : 10;
+                let scroll_height = selected_content.offsetHeight - selected_content.clientHeight > 0 ? selected_content.offsetHeight - selected_content.clientHeight : 10;
+                let right_pos_slow = document.documentElement.clientWidth - scroll_width - vert_stub_right.getBoundingClientRect().width;
+                let left_pos_slow = scroll_width + vert_stub_right.getBoundingClientRect().width;
+                let right_pos_fast = document.documentElement.clientWidth - vert_stub_right.getBoundingClientRect().width;
+                let left_pos_fast = vert_stub_right.getBoundingClientRect().width;
+                console.log(selected_content.offsetWidth - selected_content.clientWidth);
+                console.log(vert_stub_left.getBoundingClientRect().width);
+                if (e.clientX >= right_pos_slow && e.clientX < right_pos_fast) {
+                    if (scroll_interval == null || last_pos.includes("left_pos_fast")) {
+                        clearInterval(scroll_interval);
+                        last_pos = "right_pos_slow";
+                        scroll_interval = scrollOnDrag(0, tds[i].getBoundingClientRect().width, "smooth", selected_content, 250);
+                    }
+                } else if (e.clientX >= right_pos_fast) {
+                    if (scroll_interval == null || last_pos.includes("right_pos_slow")) {
+                        clearInterval(scroll_interval);
+                        last_pos = "right_pos_fast";
+                        scroll_interval = scrollOnDrag(0, tds[i].getBoundingClientRect().width, "auto", selected_content, 100);
+                    }
+                } else if (e.clientX <= left_pos_slow && e.clientX > left_pos_fast) {
+                    if (scroll_interval == null || last_pos.includes("left_pos_fast")) {
+                        clearInterval(scroll_interval);
+                        last_pos = "left_pos_slow";
+                        scroll_interval = scrollOnDrag(0, -tds[i].getBoundingClientRect().width, "smooth", selected_content, 250);
+                    }
+                } else if (e.clientX <= left_pos_fast) {
+                    if (scroll_interval == null || last_pos.includes("left_pos_slow")) {
+                        clearInterval(scroll_interval);
+                        last_pos = "left_pos_fast";
+                        scroll_interval = scrollOnDrag(0, -tds[i].getBoundingClientRect().width, "auto", selected_content, 100);
+                    }
+                } else {
+                    clearInterval(scroll_interval);
+                    scroll_interval = null;
+                    last_pos = "null";
                 }
                 
-                //console.log(document.documentElement.clientWidth + " " + document.documentElement.clientHeight);
-                //console.log(e.clientX + " " + e.clientY);
+                last_target = e.target
+                console.log(document.documentElement.clientWidth + " " + document.documentElement.clientHeight);
+                console.log(e.clientX + " " + e.clientY);
                 //console.log(e.target);
-                 //else if()
+                //else if()
                 /*td_selection.style.width = td.getBoundingClientRect().width + "px";
                 td_selection.style.height = td.getBoundingClientRect().height + "px";*/
             }
@@ -63,6 +129,14 @@ function onTdSelect(tds, ths, cnt_i) {
         tds[i].ondragstart = function() {
             return false;
         };
+    }
+
+    function scrollOnDrag(top, left, behavior, selected_content, delay) {
+        return setInterval(() => selected_content.scrollBy({
+            top: top,
+            left: left,
+            behavior: behavior
+        }), delay);
     }
 }
 
